0654bc83208b2ad9f4eaec2f36bf76f16ab08f9f
MasterAbdoTGM50 <masterabdotgm50@ymail.com>
2021-03-26 14:11:57 +0200

    Validate the role argument is actually correct in the add command

--- commands/party/add.ts
+++ commands/party/add.ts
@@ -1,11 +1,11 @@
 import { Command } from "../../handlers/command";
 import { Bot } from "../../bot";
 import { Message } from "discord.js";
-import {kickFromParty, updatePartyMsg} from "../../party/party-utils";
+import { updatePartyMsg} from "../../party/party-utils";
+import { registerInParty } from "../../party/party-reactions";
 
 import { lRole, emojis } from "../../guild.json";
-import {dm, dmError, isGuildRole} from "../../utils/dm";
-import {registerInParty, unregisterFromParty} from "../../party/party-reactions";
+import { roles } from "../../lib.json";
 
 export = class AddCommand extends Command {
 
@@ -18,16 +18,17 @@ export = class AddCommand extends Command {
             if(party !== undefined) {
                 if(args.length > 2) {
                     let role = args[1].toLowerCase();
-                    let playa = args.slice(2).join(" ").toLowerCase().trim();
-                    let gm = bot.guild.members.cache.find(gm => gm.displayName.toLowerCase().includes(playa));
-                    if(gm) {
-                        if(registerInParty(party, gm, role)) {
-                            updatePartyMsg(bot, party);
-                            message.react(`<:${emojis.confirm}>`).then();
-                            return;
+                    if(roles.includes(role)) {
+                        let playa = args.slice(2).join(" ").toLowerCase().trim();
+                        let gm = bot.guild.members.cache.find(gm => gm.displayName.toLowerCase().includes(playa));
+                        if(gm) {
+                            if(registerInParty(party, gm, role)) {
+                                updatePartyMsg(bot, party);
+                                message.react(`<:${emojis.confirm}>`).then();
+                                return;
+                            }
                         }
                     }
-
                 }
             }
         }
