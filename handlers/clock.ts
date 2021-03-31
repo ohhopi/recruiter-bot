7900128f76323a938012ff0bbee81b14a21af5ac
MasterAbdoTGM50 <masterabdotgm50@ymail.com>
2021-03-31 15:31:59 +0200

    Add the "ST" identifier to the bot's clock

--- handlers/clock.ts
+++ handlers/clock.ts
@@ -5,7 +5,7 @@ import { toFormattedTimeStr } from "../utils/time";
 export class ClockUpdater implements ITickHandler {
 
     handle(bot: Bot, now: Date) {
-        bot.client.user.setActivity(toFormattedTimeStr(now)).then();
+        bot.client.user.setActivity(`${toFormattedTimeStr(now)} ST`).then();
     }
 
 }
