8faa491b9e9d3de3dcf88851468e9a84e3aa3b8a
MasterAbdoTGM50 <masterabdotgm50@ymail.com>
2021-04-05 14:36:23 +0200

    Update chess command with Valith's amazing guide

--- commands/guides/chess.ts
+++ commands/guides/chess.ts
@@ -8,7 +8,8 @@ export = class ChessCommand extends Command {
     roles: string[] = null;
 
     handle(bot: Bot, message: Message, args: string[]) {
-        message.channel.send("https://drschess.github.io/DrsChess").then();
+        message.channel.send("Minigame Link: https://drschess.github.io/DrsChess").then();
+        message.channel.send("I used to suck at Queen Chess but after this video? I discovered I was just retarded\nhttps://www.youtube.com/watch?v=R00R_E2UtR0").then();
     }
 
 }
