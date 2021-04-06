df5931e06dd0c82565d380486cf034027197c72b
MasterAbdoTGM50 <masterabdotgm50@ymail.com>
2021-04-07 00:03:47 +0200

    Clean weather command

--- commands/weather.ts
+++ commands/weather.ts
@@ -1,13 +1,13 @@
 import { Command } from "../handlers/command";
 import { Bot } from "../bot";
-import {Message, MessageEmbed, MessageReaction, User} from "discord.js";
+import { Message, MessageEmbed, MessageReaction, User } from "discord.js";
 import { MS, S, toFormattedTimeStr } from "../utils/time";
-import {emojis} from "../guild.json";
-import {Mutex} from "async-mutex";
+
+import { emojis } from "../guild.json";
 
 export = class ForecastCommand extends Command {
 
-    aliases: string[] = ["forecast", "weather"];
+    aliases: string[] = [ "forecast", "weather" ];
     roles: string[] = null;
 
     handle(bot: Bot, message: Message, args: string[]) {
@@ -23,30 +23,36 @@ export = class ForecastCommand extends Command {
         let date = new Date();
         let forecasts: IForecast[] = [];
 
-        forecasts.push({ weather: forecast(date), start: toFormattedTimeStr(date) });
+        forecasts.push({ weather: forecast(date), start: toFormattedTimeStr(date), duration: 0 });
 
         for(let i = 0; i < steps; ++i) {
-            if(last(forecasts).weather === forecast(date)) { stepInTime(date, 1); }
-            else {
-                last(forecasts).end = toFormattedTimeStr(date);
-                forecasts.push({ weather: forecast(date), start: toFormattedTimeStr(date) });
+            let f = forecast(date);
+            if(last(forecasts).weather === f) {
+                ++last(forecasts).duration;
+                stepInTime(date, 1);
+                continue;
             }
+            forecasts.push({ weather: f, start: toFormattedTimeStr(date), duration: 0 });
         }
 
-        while(last(forecasts).weather === forecast(date)) { stepInTime(date, 1); }
-        last(forecasts).end = toFormattedTimeStr(date);
+        while(last(forecasts).weather === forecast(date)) {
+            ++last(forecasts).duration;
+            stepInTime(date, 1);
+        }
 
         message.channel.send(buildForecastEmbed(forecasts)).then(async msg => {
 
             let pages = Math.ceil(forecasts.length / 24);
             if(pages === 1) { return; }
-            let rCollector = msg.createReactionCollector(filterReactions, { time: 15 * S * MS });
+            let rCollector = msg.createReactionCollector(createReactionFilter(message.author), { time: 15 * S * MS });
             rCollector.forecasts = forecasts;
             rCollector.page = 1;
             rCollector.pages = pages;
 
             rCollector.on("collect", async (reaction: MessageReaction, user: User) => {
-                reaction.users.remove(user).then();
+                if(reaction.message.channel.type !== "dm") {
+                    reaction.users.remove(user).then();
+                }
                 if(reaction.emoji.name === "⬅️") {
                     if(rCollector.page === 1) { return; }
                     --rCollector.page;
@@ -76,8 +82,21 @@ declare module "discord.js" {
 }
 
 function last<T>(arr: T[]) { return arr[arr.length - 1]; }
-function stepInTime(date: Date, mins: number) {
-    date.setTime(date.getTime() + (mins * S * MS));
+function stepInTime(date: Date, mins: number) { date.setTime(date.getTime() + (mins * S * MS)); }
+
+interface IForecast {
+    weather: string,
+    start: string,
+    duration: number
+}
+
+function forecast(date: Date): string {
+    let chance = calculateForecastTarget(date);
+    if (chance < 52) { return `Fair`; }
+    if (chance < 64) { return `Rain`; }
+    if (chance < 76) { return `Wind`; }
+    if (chance < 88) { return `Thunder`; }
+    return `Dust`;
 }
 
 function calculateForecastTarget(date: Date): number {
@@ -101,21 +120,6 @@ function calculateForecastTarget(date: Date): number {
     return step2 % 100;
 }
 
-interface IForecast {
-    weather: string,
-    start: string,
-    end?: string
-}
-
-function forecast(date: Date): string {
-    let chance = calculateForecastTarget(date);
-    if (chance < 52) { return `<:${emojis.fair}> Fair`; }
-    if (chance < 64) { return `<:${emojis.rain}> Rain`; }
-    if (chance < 76) { return `<:${emojis.wind}> Wind`; }
-    if (chance < 88) { return `<:${emojis.thunder}> Thunder`; }
-    return `<:${emojis.dust}> Dust`;
-}
-
 function buildForecastEmbed(forecasts: IForecast[], page= 1) {
     let embed = new MessageEmbed().setTitle(`Bozjan Weather Forecast #${page}`).setColor("ORANGE");
 
@@ -123,7 +127,9 @@ function buildForecastEmbed(forecasts: IForecast[], page= 1) {
     let end = Math.min(forecasts.length, page * 24);
 
     for(let i = start; i < end; ++i) {
-        embed.addField(`${forecasts[i].start} ST to ${forecasts[i].end} ST`, forecasts[i].weather, true);
+        let title = `At ${forecasts[i].start} ST for ${forecasts[i].duration} Mins`;
+        let icons = Array(Math.ceil(forecasts[i].duration / 24)).fill(`<:${emojis[forecasts[i].weather.toLowerCase()]}>`).join(" ");
+        embed.addField(title, icons + " " + forecasts[i].weather, true);
     }
 
     embed.setFooter("I will auto delete this menu once it's 15 minutes old");
@@ -131,8 +137,10 @@ function buildForecastEmbed(forecasts: IForecast[], page= 1) {
     return embed;
 }
 
-function filterReactions(reaction: MessageReaction, user: User) {
-    if(user.bot) { return false; }
-    if(reaction.emoji.name !== "⬅️" && reaction.emoji.name !== "➡️") { reaction.remove().then(); return false; }
-    return true;
+function createReactionFilter(user: User) {
+    return (r: MessageReaction, u: User): boolean => {
+        if(user.id !== u.id) { return false; }
+        if(r.emoji.name !== "⬅️" && r.emoji.name !== "➡️") { r.remove().then(); return false; }
+        return true;
+    }
 }
